f957ce84a3b48dc3fc7861028953715f51c8183d
Daniel Sepulveda <56978447+DanielSI21@users.noreply.github.com>
2022-08-18 10:49:06 -0600

    Actividades clase 18 de agosto de 2022

--- Unidad 2/index.php
+++ Unidad 2/index.php
@@ -100,51 +100,192 @@
         // });
 
         //actividad 13 circulos
-        let r = 255;
-        let g = 255;
-        let b = 255;
-        let color = "rgba(" + r + "," + g + "," + b + ", 0.3)";
+        // let r = 255;
+        // let g = 255;
+        // let b = 255;
+        // let color = "rgba(" + r + "," + g + "," + b + ", 0.3)";
 
-        cv.addEventListener('click', function(e){
-            let x = e.offsetX;
-            let y = e.offsetY;
-            ctx.beginPath();
-            ctx.arc(x, y, 40, 0, 2*Math.PI);
-            ctx.fillStyle = "rgba(" + r + "," + g + "," + b + ", 0.3)";
-            ctx.fill();
-            ctx.strokeStyle = "rgb(0,0,0)";
-            ctx.stroke();
-        });
+        // cv.addEventListener('click', function(e){
+        //     let x = e.offsetX;
+        //     let y = e.offsetY;
+        //     ctx.beginPath();
+        //     ctx.arc(x, y, 40, 0, 2*Math.PI);
+        //     ctx.fillStyle = "rgba(" + r + "," + g + "," + b + ", 0.3)";
+        //     ctx.fill();
+        //     ctx.strokeStyle = "rgb(0,0,0)";
+        //     ctx.stroke();
+        // });
 
-        cv.addEventListener('mouseover', function(e){
-            r = Math.floor(Math.random() * (255 - 0) + 1);
-            g = Math.floor(Math.random() * (255 - 0) + 1);
-            b = Math.floor(Math.random() * (255 - 0) + 1);
-        });
+        // cv.addEventListener('mouseover', function(e){
+        //     r = Math.floor(Math.random() * (255 - 0) + 1);
+        //     g = Math.floor(Math.random() * (255 - 0) + 1);
+        //     b = Math.floor(Math.random() * (255 - 0) + 1);
+        // });
 
-        
-        // pruebas
+        //actividad 14
+
+        // let r = 255;
+        // let g = 255;
+        // let b = 255;
+        // var fig = 'arc';
+        // let color = "rgba(" + r + "," + g + "," + b + ", 0.3)";
+
+        // cv.addEventListener('click', function(e){
+        //     let x = e.offsetX;
+        //     let y = e.offsetY;
+        //     ctx.beginPath();
+        //     ctx.fillStyle = "rgba(" + r + "," + g + "," + b + ", 0.3)";
+        //     ctx.strokeStyle = "rgb(0,0,0)";
+
+        //     if (fig == 'arc'){
+        //         ctx.arc(x, y, 40, 0, 2*Math.PI);
+        //         ctx.fill();
+        //         ctx.stroke();
+        //     }else{
+        //         ctx.fillRect(x, y, 80, 80);
+        //         ctx.strokeRect(x, y, 80, 80);
+        //     }
+        //     console.log(fig);
+        // });
+
+        // cv.addEventListener('mouseover', function(e){
+        //     r = Math.floor(Math.random() * (255 - 0) + 1);
+        //     g = Math.floor(Math.random() * (255 - 0) + 1);
+        //     b = Math.floor(Math.random() * (255 - 0) + 1);
+        // });
+
+        // cv.addEventListener('mouseout', function(e){
+        //     fig = (fig=='arc') ? 'rec': 'arc';
+        // });
+
+        //pruebas
+        // // cv.onmousemove = function(e){
+        // //     r = Math.floor(Math.random() * (255 - 0) + 1);
+        // //     g = Math.floor(Math.random() * (255 - 0) + 1);
+        // //     b = Math.floor(Math.random() * (255 - 0) + 1);
+        // // }
+
+        //actividad 15
+        // let r = 255;
+        // let g = 255;
+        // let b = 255;
+        // var fig = 'arc';
+        // let color = "rgba(" + r + "," + g + "," + b + ", 0.3)";
+
+        // cv.addEventListener('click', function(e){
+        //     let x = e.offsetX;
+        //     let y = e.offsetY;
+        //     ctx.beginPath();
+        //     ctx.fillStyle = "rgba(" + r + "," + g + "," + b + ", 0.3)";
+
+        //     if (fig == 'arc'){
+        //         ctx.arc(x, y, 40, 0, 2*Math.PI);
+        //         ctx.fill();
+        //         ctx.stroke();
+        //     }else{
+        //         ctx.strokeRect(x-40, y-40, 80, 80);
+        //         ctx.fillRect(x-40, y-40, 80, 80);
+        //     }
+        // });
+
         // cv.onmousemove = function(e){
         //     r = Math.floor(Math.random() * (255 - 0) + 1);
         //     g = Math.floor(Math.random() * (255 - 0) + 1);
         //     b = Math.floor(Math.random() * (255 - 0) + 1);
-        // }
-
-        // cv.onmousemove = function(e){
+        //     fig = (fig=='arc') ? 'rec': 'arc';
         //     x = e.pageX;
         //     y = e.pageY;
-        //     let r = Math.floor(Math.random() * (255 - 0) + 1);
-        //     let g = Math.floor(Math.random() * (255 - 0) + 1);
-        //     let b = Math.floor(Math.random() * (255 - 0) + 1);
-        //     ctx.fillStyle = "rgba(" + r + "," + g + "," + b + ", 0.3)";
+        //     ctx.fillStyle = "black";
         //     ctx.beginPath();
-        //     ctx.arc(x, y, 40, 0, 2*Math.PI);
-        //     ctx.fill();
-        //     ctx.strokeStyle = "rgb(0,0,0)";
+        //     ctx.fillRect(x, y, 5, 5);
         //     ctx.stroke();
+        //     ctx.fill();
         // }
 
+        //actividad 16
+        // let r = 255;
+        // let g = 255;
+        // let b = 255;
+        // var fig = 'arc';
+        // var press = false;
+        // let color = "rgba(" + r + "," + g + "," + b + ", 0.3)";
 
+        // cv.addEventListener('click', function(e){
+        //     let x = e.offsetX;
+        //     let y = e.offsetY;
+        //     ctx.beginPath();
+        //     ctx.fillStyle = "rgba(" + r + "," + g + "," + b + ", 0.3)";
+
+        //     if (fig == 'arc'){
+        //         ctx.arc(x, y, 40, 0, 2*Math.PI);
+        //         ctx.fill();
+        //         ctx.stroke();
+        //     }else{
+        //         ctx.strokeRect(x-40, y-40, 80, 80);
+        //         ctx.fillRect(x-40, y-40, 80, 80);
+        //     }
+        // });
+
+        // cv.onmousemove = function(e){
+        //     r = Math.floor(Math.random() * (255 - 0) + 1);
+        //     g = Math.floor(Math.random() * (255 - 0) + 1);
+        //     b = Math.floor(Math.random() * (255 - 0) + 1);
+        //     fig = (fig=='arc') ? 'rec': 'arc';
+        //     x = e.pageX;
+        //     y = e.pageY;
+        //     if (press){
+        //         ctx.fillStyle = "black";
+        //         ctx.fillRect(x, y, 5, 5);
+        //     }
+        // }
+
+        // cv.addEventListener('mousedown', function(e){
+        //     press = true;
+        // });
+
+        // cv.addEventListener('mouseup', function(e){
+        //     press = false;
+        // });
+
+        //actividad 17
+        var super_x = 0, super_y = 0;
+        
+        document.addEventListener('keydown', function(e){
+            console.log(e);
+            if (e.key == "ArrowUp" || e.key == "w"){
+                super_y-=7;
+            }
+            if (e.key == "ArrowDown" || e.key == "s"){
+                super_y+=7;
+            }
+            if (e.key == "ArrowLeft" || e.key == "a"){
+                super_x-=7;
+            }
+            if (e.key == "ArrowRight" || e.key == "d"){
+                super_x+=7;
+            }       
+            paint();          
+        });
+
+        function paint(){
+            ctx.fillStyle = "rgb(55, 55, 55)";
+            ctx.fillRect(0,0, 500, 500);
+            ctx.fillStyle = "red";
+            ctx.fillRect(super_x, super_y, 40, 40);
+            ctx.strokeRect(super_x, super_y, 40, 40);
+            // var img = document.getElementById('imagen');
+            // ctx.drawImage(img, super_x, super_y, 100, 100);
+        }
+
+        //actividad 18
+        // cv.addEventListener('click', function(e){
+        //     let x = e.offsetX;
+        //     let y = e.offsetY;
+        //     let texto = x + "," + y;
+        //     ctx.fillStyle = "rgb(0,0,0)";
+        //     ctx.font = "10px Arial";
+        //     ctx.fillText(texto, x, y);
+        // });
     </script>
 </body>
 </html>
