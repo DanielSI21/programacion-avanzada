a10c520c1aa22410230b4a887824f4ba1e298018
Daniel Sepulveda <cdsi21_@hotmail.com>
2022-09-20 09:59:36 -0600

    Actividad 5 - 20 de septiembre de 2022

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso al panel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/main.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row align-items-center d-flex justify-content-center">
+            <div class="col-md-8">
+                <form>
+                    <fieldset>
+                        <legend>
+                            Datos de acceso
+                        </legend>
+
+                        <label>
+                            Email:
+                        </label>
+
+                        <input type="text" name="" placeholder="write here">
+
+                        <label>
+                            Passowrd:
+                        </label>
+                        <input type="password" name="" placeholder="*****" name="">
+
+                        <button type="submit">
+                            Acceder
+                        </button>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
