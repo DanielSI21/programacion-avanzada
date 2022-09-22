7a7e73ae74fccc5044ba1bae21757f0473b84024
Daniel Sepulveda <cdsi21_@hotmail.com>
2022-09-22 10:08:09 -0600

    Actividad 6 - 22 de septiembre 2022

--- index.php
+++ index.php
@@ -7,9 +7,12 @@
     <title>Acceso al panel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="public/css/main.css"> -->
+    <style>
+        
+    </style>
 </head>
 <body>
-    <div class="container">
+    <!-- <div class="container">
         <div class="row align-items-center d-flex justify-content-center">
             <div class="col-md-8">
                 <form>
@@ -36,7 +39,43 @@
                 </form>
             </div>
         </div>
+    </div> -->
+    <div class="container">
+        <section>
+            <div class="row justify-center-md-center align-items-center">
+                <div class="col-md-6 col-lg-4 col-sm-12 login">
+                    <h1>Acceso al panel</h1>
+                    <p>
+                        Bienvenido de vuelta
+                    </p>
+
+                    <form method="get" class="form" action="products">
+                        <div>
+                            <label for="">
+                                Correo electronico
+                            </label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">@</span>
+                                <input type="text" class="form-control" placeholder="Correo" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        <div>
+                            <label for="">
+                                Contraseña
+                            </label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">@</span>
+                                <input type="text" class="form-control" placeholder="Contraseña" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        
+                        <button type="submit" class="btn btn-primary col-12 mb-3">Acceder</button>
+                    </form>
+                </div>
+            </div>
+        </section>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
 </html>
